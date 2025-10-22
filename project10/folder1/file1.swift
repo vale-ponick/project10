3be0cbd6b4d9883b239218aa8f488f61ea5bae59
Valery Ponomareva <vale.ponick@gmail.com>
2025-10-22 22:21:12 +0700

    add: first commit

---
+++ project10/folder1/file1.swift
@@ -0,0 +1,10 @@
+//
+//  file1.swift
+//  project10
+//
+//  Created by Валерия Пономарева on 22.10.2025.
+//
+
+import Foundation
+
+print("Hello!")
---
+++ project10/project10/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  project10
+//
+//  Created by Валерия Пономарева on 22.10.2025.
+//
+
+import Foundation
+
+print("Hello, World!")
+
